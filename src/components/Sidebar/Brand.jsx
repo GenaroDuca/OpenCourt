cb0b4950a985b341af6371886fd1a8cb4917833d
Genaro Duca <genaroduca10@gmail.com>
2026-02-20 01:17:49 -0300

    cambio de logo

--- src/components/Sidebar/Brand.jsx
+++ src/components/Sidebar/Brand.jsx
@@ -2,14 +2,8 @@ import logo from "../../../public/OpenCourt.png";
 
 export default function Brand() {
   return (
-    <div className="text-center items-center gap-2 hidden md:flex">
-      <img src={logo} alt="Logo" className="w-16 h-16" />
-      <div>
-        <h1 className="text-2xl font-bold text-left text-white">OpenCourt</h1>
-        <p className="text-sm text-text-dark-mode text-gray-500">
-          Panel Administrativo
-        </p>
-      </div>
+    <div className="text-center items-center hidden md:flex flex-col">
+      <img src={logo} alt="Logo" className="w-35 h-auto" />
     </div>
   );
 }
