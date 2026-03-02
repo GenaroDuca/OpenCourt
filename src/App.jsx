78e63bff6eb592b16e20ead31920076efd9c42f8
Genaro Duca <genaroduca10@gmail.com>
2026-03-02 12:31:15 -0300

    Componente <Analytics /> de vercel

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,39 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AdminPanel from "./pages/AdminPanel";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
-    <Router>
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          style: {
-            background: "var(--color-background-card-color)",
-            color: "var(--color-text-color)",
-            border: "1px solid var(--color-border-color)",
-            padding: "12px 16px",
-            borderRadius: "var(--radius-lg)",
-          },
-          className: "text-xs md:text-base",
-        }}
-      />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/admin/*"
-          element={
-            <ProtectedRoute>
-              <AdminPanel />
-            </ProtectedRoute>
-          }
+    <>
+      <Analytics />
+      <Router>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: "var(--color-background-card-color)",
+              color: "var(--color-text-color)",
+              border: "1px solid var(--color-border-color)",
+              padding: "12px 16px",
+              borderRadius: "var(--radius-lg)",
+            },
+            className: "text-xs md:text-base",
+          }}
         />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin/*"
+            element={
+              <ProtectedRoute>
+                <AdminPanel />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
